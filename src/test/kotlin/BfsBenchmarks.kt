1fb57e9b9fe9781e9afd4caa8701eebd07de8d83
Evgeniy.Zhelenskiy <zhelenskiy2000@yandex.ru>
2021-12-18 18:21:07 +0300

    Heating is added to BFS benchmark

--- src/test/kotlin/BfsBenchmarks.kt
+++ src/test/kotlin/BfsBenchmarks.kt
@@ -6,7 +6,7 @@ import kotlin.test.assertEquals
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
-private const val edgeSize = 500
+private const val edgeSize = 450
 
 private fun checkLogCorrectness(log: IntArray) {
     assertEquals(log.size, edgeSize * edgeSize * edgeSize)
@@ -18,11 +18,19 @@ private fun checkLogCorrectness(log: IntArray) {
 
 @ExperimentalTime
 fun main() { // run with -Xmx20G
+    println("Initializing...")
     val lazyList = CubeGraphLazyList(edgeSize)
     val graph = Graph(lazyList.toList() /* make eager */)
     val log = IntArray(graph.size)
     val ratios = ArrayList<Double>()
-    repeat(5) {
+    val heatingCount = 2
+    val benchmarkCount = 5
+    repeat(heatingCount + benchmarkCount) {
+        if (it == heatingCount) {
+            println("Benchmark:")
+        } else if (it == 0) {
+            println("Heating...")
+        }
         System.gc()
         log.fill(0)
         val seq = measureTime {
@@ -36,8 +44,10 @@ fun main() { // run with -Xmx20G
             graph.parallelBfs(0) { oldNode, newNode -> log[newNode] = log[oldNode] + 1 }
         }
         checkLogCorrectness(log)
-        ratios.add(seq / par)
-        println("Seq: $seq, Par: $par, Ratio: ${ratios.last()}")
+        if (it >= heatingCount) {
+            ratios.add(seq / par)
+            println("Seq: $seq, Par: $par, Ratio: ${ratios.last()}")
+        }
     }
     println()
     println("Average ratio: ${ratios.average()}")
